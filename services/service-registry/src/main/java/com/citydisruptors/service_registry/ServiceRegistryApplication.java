649630ac49b8bc19b953ea0ce8cc51cf3cda86ce
lukaszpelikan <l.pelikan@fairplacefinance.pl>
2026-04-07 15:30:41 +0200

    Initialize service reigstry and config server

---
+++ services/service-registry/src/main/java/com/citydisruptors/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.citydisruptors.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
